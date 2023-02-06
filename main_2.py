b26c0c026d77a9aa631b150759052279cf74bb8f
pavelakomazov <yakomazov.pavel@yandex.ru>
2023-02-07 00:37:00 +0300

    very excited!

--- main_2.py
+++ main_2.py
@@ -1,7 +1,9 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+import os
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from MainWindow_V2 import Ui_MainWindow
 from PySide6 import QtUiTools
+from paths import BASE_DIR, ALL_PATHS, save_all_paths
 
 
 class MainWindow(QMainWindow):
@@ -9,6 +11,27 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.action_select_temperature_template.triggered.connect(
+            # self.action_select_temperature_template)
+            lambda: self.action_select_temperature_template(key="file_dir_temperature_template"))
+        self.ui.action_select_shared_save_directory.triggered.connect(
+            self.action_select_shared_save_directory)
+
+    def action_select_temperature_template(self, key):
+        dir = QFileDialog.getOpenFileName(
+            self, "Open Directory", BASE_DIR, "Word (*.docx)")
+        print(dir[0])
+        ALL_PATHS[key] = dir[0]
+        save_all_paths()
+
+
+    def action_select_shared_save_directory(self):
+        dir = QFileDialog.getExistingDirectory(
+            self, "Open Directory", BASE_DIR)
+        print(dir)
+        ALL_PATHS["dir_shared_save_directory"] = dir
+        save_all_paths()
+
 
 
 if __name__ == "__main__":
---
+++ paths.py
@@ -0,0 +1,60 @@
+import json
+import os
+
+
+BASE_DIR = os.getcwd()
+DOC_NAME = 'ALL_PATHS.json'
+ALL_PATHS = {}
+PATH_KEYS = (
+	
+	# Пути для дирректорий сохранения
+	"dir_shared_save_directory",
+	"dir_ol_save_directory",
+	"dir_tsp_save_directory",
+	"dir_io_save_directory",
+	"dir_kj_save_directory",
+	"dir_specifaication_save_directory",
+
+	# Пути для файлов шаблонов
+	"file_dir_temperature_template",
+	"file_dir_pressure_template",
+	"file_dir_flow_template",
+	"file_dir_evel_template",
+	"file_dir_analyzer_template",
+	"file_dir_control_valve_template",
+	"file_dir_shut_off_valve_template",
+	"file_dir_environments_descriptions",
+	"file_dir_tsp_template",
+	"file_dir_io_template",
+	"file_dir_kj_template",
+	"file_dir_specification_template"
+)
+
+full_dir_all_paths = os.path.join(BASE_DIR, DOC_NAME)
+
+
+# Если .json файл существует
+
+if os.path.exists(full_dir_all_paths):
+	with open(full_dir_all_paths, 'r') as file:
+		ALL_PATHS = json.load(file)
+
+		# Проверка ключей полученного dict
+
+		if not tuple(ALL_PATHS.keys()) == PATH_KEYS:
+
+			# Если ключи не соответствуют кортежу PATH_KEYS
+
+			ALL_PATHS = dict.fromkeys(PATH_KEYS)
+
+# Если .json файл не существует
+
+else:
+	ALL_PATHS = dict.fromkeys(PATH_KEYS)
+
+
+# Сохранить переменную ALL_PATHS в .json файл
+
+def save_all_paths():
+	with open(full_dir_all_paths, 'w') as f:
+		json.dump(ALL_PATHS, f)
