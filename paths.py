7af415ff832b43b422c37d782ceb33ba87e5c2bf
михан <>
2023-02-19 19:15:30 +0300

    homework

--- paths.py
+++ paths.py
@@ -1,61 +1,94 @@
 import json
 import os
 
+from docx import Document
 
 BASE_DIR = os.getcwd()
-DOC_NAME = 'ALL_PATHS.json'
-ALL_PATHS = {}
-PATH_KEYS = (
-	
-	# Пути для дирректорий сохранения
-	"dir_shared_save_directory",
-	"dir_ol_save_directory",
-	"dir_tsp_save_directory",
-	"dir_io_save_directory",
-	"dir_kj_save_directory",
-	"dir_specifaication_save_directory",
+DOC_NAME = 'SETTINGS.json'
 
-	# Пути для файлов шаблонов
-	"file_dir_temperature_template",
-	"file_dir_pressure_template",
-	"file_dir_flow_template",
-	"file_dir_level_template",
-	"file_dir_analyzer_template",
-	"file_dir_control_valve_template",
-	"file_dir_shut_off_valve_template",
-	"file_dir_environments_descriptions",
-	"file_dir_tsp_template",
-	"file_dir_io_template",
-	"file_dir_kj_template",
-	"file_dir_specification_template"
+PATH_KEYS = (
+
+    # Пути для дирректорий сохранения
+    "dir_shared_save_directory",
+    "dir_ol_save_directory",
+    "dir_tsp_save_directory",
+    "dir_io_save_directory",
+    "dir_kj_save_directory",
+    "dir_specifaication_save_directory",
+
+    # Пути для файлов шаблонов
+    "file_dir_temperature_template",
+    "file_dir_pressure_template",
+    "file_dir_flow_template",
+    "file_dir_level_template",
+    "file_dir_analyzer_template",
+    "file_dir_control_valve_template",
+    "file_dir_shut_off_valve_template",
+    "file_dir_environments_descriptions",
+    "file_dir_tsp_template",
+    "file_dir_io_template",
+    "file_dir_kj_template",
+    "file_dir_specification_template",
+
+    # Имена для выгружаемых файлов
+    "file_name_temperature_ol",
+    "file_name_pressure_ol",
+    "file_name_flow_ol",
+    "file_name_level_ol",
+    "file_name_analyzer_ol",
+    "file_name_control_valve_ol",
+    "file_name_shut_off_valve_ol",
+    "file_name_tsp",
+    "file_name_io",
+    "file_name_kj",
+    "file_name_specification",
+
+    # Шаблоны
+    "template_temperature",
+    "template_pressure",
+    "template_flow",
+    "template_level",
+    "template_control_valve",
+    "template_shut_off_valve",
+    "template_tsp",
+    "template_io",
+    "template_kj",
+    "template_specification"
+
 )
 
-full_dir_all_paths = os.path.join(BASE_DIR, DOC_NAME)
+
+def create_json_file(path, keys):
+    # Если .json файл существует
+
+    if os.path.exists(path):
+        with open(path, 'r', encoding='cp1251') as file:
+            json_file = json.load(file)
+
+            # Проверка ключей полученного dict
+
+            if tuple(json_file.keys()) == keys:
+                return json_file
+                # Если ключи не соответствуют кортежу
+            else:
+                return dict.fromkeys(keys)
+
+    # Если .json файл не существует
+
+    else:
+        return dict.fromkeys(keys)
 
 
-# Если .json файл существует
-
-if os.path.exists(full_dir_all_paths):
-	with open(full_dir_all_paths, 'r') as file:
-		ALL_PATHS = json.load(file)
-		# print(ALL_PATHS)
-
-		# Проверка ключей полученного dict
-
-		if not tuple(ALL_PATHS.keys()) == PATH_KEYS:
-
-			# Если ключи не соответствуют кортежу PATH_KEYS
-
-			ALL_PATHS = dict.fromkeys(PATH_KEYS)
-
-# Если .json файл не существует
-
-else:
-	ALL_PATHS = dict.fromkeys(PATH_KEYS)
+def save_json_file(path_to_file, dictionary):
+    with open(path_to_file, 'w') as f:
+        # json.dump(dictionary, f, cls=None, ensure_ascii=False, sort_keys=False, indent=4, separators=(',', ': '))
+        json.dump(dictionary, f, ensure_ascii=False)
 
 
-# Сохранить переменную ALL_PATHS в .json файл
+# full_dir_all_settings = os.path.join(BASE_DIR, DOC_NAME)
 
-def save_all_paths():
-	with open(full_dir_all_paths, 'w') as f:
-		json.dump(ALL_PATHS, f)
+SETTINGS = create_json_file(os.path.join(BASE_DIR, DOC_NAME), PATH_KEYS)
+# a = SETTINGS['template_temperature']
+# print(a['Позиция'])
+
+# save_json_file(os.path.join(BASE_DIR, DOC_NAME), SETTINGS)
--- unloading_to_word.py
+++ unloading_to_word.py
@@ -1,14 +1,20 @@
+import os
+
 from docx import Document
 from copy import deepcopy
 from data_engine import getTemptureForOL, getPressureForOL, getFlowForOL, getLevelForOL
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Pt
-from data_engine import templates
+from paths import SETTINGS
+
+
+# from data_engine import templates
 
 def delete_paragraph(paragraph):
     p = paragraph._element
     p.getparent().remove(p)
     p._p = p._element = None
+
 
 def cell_style(cell, size):
     paragra_ph = cell.paragraphs[0]
@@ -18,22 +24,71 @@ def cell_style(cell, size):
     font.size = Pt(size)
 
 
-def unload(parametr, df_table_list_positions, df_ol_table):
-    global savePath
-    if parametr == 'Температура':
-        document = Document(f'Шаблоны/ОЛ/Температура.docx')
-        savePath = '193-РП-АТХ1.ОЛ1'
-    elif parametr == 'Давление':
-        document = Document(f'Шаблоны/ОЛ/Давление.docx')
-        savePath = '193-РП-АТХ1.ОЛ2'
-    elif parametr == 'Расход':
-        document = Document(f'Шаблоны/ОЛ/Расход.docx')
-        savePath = '193-РП-АТХ1.ОЛ3'
-    elif parametr == 'Уровень':
-        document = Document(f'Шаблоны/ОЛ/Уровень.docx')
-        savePath = '193-РП-АТХ1.ОЛ4'
-    else:
-        return
+# def unload(parametr, df_table_list_positions, df_ol_table):
+#     global savePath
+#     if parametr == 'Температура':
+#         document = Document(f'Выгрузка/Шаблоны/ОЛ/Температура.docx')
+#         savePath = '193-РП-АТХ1.ОЛ1'
+#     elif parametr == 'Давление':
+#         document = Document(f'Выгрузка/Шаблоны/ОЛ/Давление.docx')
+#         savePath = '193-РП-АТХ1.ОЛ2'
+#     elif parametr == 'Расход':
+#         document = Document(f'Выгрузка/Шаблоны/ОЛ/Расход.docx')
+#         savePath = '193-РП-АТХ1.ОЛ3'
+#     elif parametr == 'Уровень':
+#         document = Document(f'Выгрузка/Шаблоны/ОЛ/Уровень.docx')
+#         savePath = '193-РП-АТХ1.ОЛ4'
+#     else:
+#         return
+#     table_list_positions = document.tables[-2]
+#
+#     for index, row in df_table_list_positions.iterrows():
+#         tb_row = table_list_positions.add_row()
+#         cells = tb_row.cells
+#
+#         for _ in range(0, len(cells)):
+#             cell = cells[_]
+#             cell.text = str(row.iloc[_])
+#             cell_style(cell, 10)
+#
+#     ol_table = document.tables[-1]
+#     last_humans_paragraph = document.paragraphs[-2]
+#
+#     for index, row in df_ol_table.iterrows():
+#         document.add_page_break()
+#         # paragraph = document.add_paragraph()
+#         paragraph = document.paragraphs[-1]
+#         paragraph._p.addnext(deepcopy(ol_table._tbl))
+#         cur_table = document.tables[-1]
+#         for item in templates[parametr].keys():
+#             n_row, n_col = templates[parametr][item]
+#             cell = cur_table.cell(n_row, n_col)
+#             cell.text = str(row[item])
+#             if n_col > 4:
+#                 cell_style(cell, 10)
+#     ol_table._element.getparent().remove(ol_table._element)
+#     delete_paragraph(last_humans_paragraph)
+#
+#     document_env = Document(f'Шаблоны/ОЛ/Среды.docx')
+#     table_env = document_env.tables[-1]
+#
+#     document.add_page_break()
+#     paragraph = document.add_paragraph('Приложение 1')
+#
+#     paragraph.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+#     paragraph._p.addnext(deepcopy(table_env._tbl))
+#
+#
+#     document.save(f'Выгрузка/Опросные листы/{savePath}.docx')
+
+def unload_ol(template_path, template, save_path, save_name, df_table_list_positions, df_ol_table):
+    full_save_path = os.path.join(save_path, save_name + '.docx')
+
+    # if not (os.path.exists(template_path)):
+    #     print(f'Директории {template_path} для шаблона "{template_name}" не существует')
+    #     return
+
+    document = Document(template_path)
     table_list_positions = document.tables[-2]
 
     for index, row in df_table_list_positions.iterrows():
@@ -54,8 +109,8 @@ def unload(parametr, df_table_list_positions, df_ol_table):
         paragraph = document.paragraphs[-1]
         paragraph._p.addnext(deepcopy(ol_table._tbl))
         cur_table = document.tables[-1]
-        for item in templates[parametr].keys():
-            n_row, n_col = templates[parametr][item]
+        for item in template.keys():
+            n_row, n_col = template[item]
             cell = cur_table.cell(n_row, n_col)
             cell.text = str(row[item])
             if n_col > 4:
@@ -63,7 +118,7 @@ def unload(parametr, df_table_list_positions, df_ol_table):
     ol_table._element.getparent().remove(ol_table._element)
     delete_paragraph(last_humans_paragraph)
 
-    document_env = Document(f'Шаблоны/ОЛ/Среды.docx')
+    document_env = Document(SETTINGS["file_dir_environments_descriptions"])
     table_env = document_env.tables[-1]
 
     document.add_page_break()
@@ -72,5 +127,15 @@ def unload(parametr, df_table_list_positions, df_ol_table):
     paragraph.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
     paragraph._p.addnext(deepcopy(table_env._tbl))
 
+    document.save(full_save_path)
 
-    document.save(f'Выгрузка/Опросные листы/{savePath}.docx')
+
+t_ol = getTemptureForOL()
+print('HI')
+# unload_ol(template_path, template_name, save_path, save_name, df_table_list_positions, df_ol_table)
+unload_ol(template_path=SETTINGS["file_dir_temperature_template"],
+          template=SETTINGS["template_temperature"],
+          save_path=SETTINGS["dir_ol_save_directory"],
+          save_name=SETTINGS["file_name_temperature_ol"],
+          df_table_list_positions=t_ol[0],
+          df_ol_table=t_ol[1])
